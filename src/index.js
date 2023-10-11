33928ab30ab16322d8908811aa6d2f68357501af
oro kang <dkang8@ncsu.edu>
2023-10-10 20:31:24 -0400

    implemented changing screen

--- src/index.js
+++ src/index.js
@@ -6,44 +6,16 @@ const ctx = canvas2d.getContext('2d');
 const colorList = ["#81dbc5", "#76b6db", "#7691e3", "#7d76e3"]
 let flag = false;
 let dim = 1;
+let reachedTopFlag = false;
 
 
-if ("mediaSession" in navigator) {
-    navigator.mediaSession.metadata = new MediaMetadata({
-      title: "Unforgettable",
-      artist: "Nat King Cole",
-      album: "The Ultimate Collection (Remastered)",
-      artwork: [
-        {
-          src: "https://dummyimage.com/96x96",
-          sizes: "96x96",
-          type: "image/png",
-        },
-        {
-          src: "https://dummyimage.com/128x128",
-          sizes: "128x128",
-          type: "image/png",
-        },
-        {
-          src: "https://dummyimage.com/192x192",
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          src: "https://dummyimage.com/256x256",
-          sizes: "256x256",
-          type: "image/png",
-        },
-        ]
-    })
-};
 
 let userState = {level:null, duration: null, }
 
 const stream = canvas2d.captureStream();
 target.srcObject = stream;
 
-anim();
+
 
 const btn = document.getElementById('pip');
 if( target.requestPictureInPicture ) {
@@ -99,33 +71,62 @@ function on(){
 }
 
 
+function paintNextLevel(){
 
-
-function anim(timestamp) {
-    //ctx.fillStyle = "white";
-    //ctx.fillRect( 0, 0, canvas2d.width, canvas2d.height );
-    ctx.rect(0, y, canvas2d.width, canvas2d.height);
-    if(!flag){
-        ctx.fillStyle = colorList[0];
-        ctx.fill();
-    }else{
-        ctx.fillRect(0, y, canvas2d.width, canvas2d.height);
-        ctx.fillStyle = colorList[2];
-       
-       //console.log((timestamp - last)/10, y);
-    }
-    console.log("dim: ", dim, "y: ", y);
-   
-
-    /*
-    y -= (timestamp - last) / 10;
-    last = timestamp;
-    */
-    y -= dim;
-    dim += 0.0001;
-    
-    ctx.fillStyle = "black";
-    ctx.fillText( new Date().toTimeString().split(' ')[0], canvas2d.width / 2, canvas2d.height / 2 );
-
-    requestAnimationFrame( anim );
 }
+
+
+var paint = {
+
+    backgroundColor: "white",
+
+    paintStaticBackground: function(color){
+      ctx.fillStyle = color; 
+      ctx.fillRect(0, 0, canvas2d.width, canvas2d.height);
+    },
+    anim:  function (timestamp){
+      //setting default background 
+
+      //ctx.fillStyle = "white"; 
+      //ctx.fillRect(0, 0, canvas2d.width, canvas2d.height);
+      this.paintStaticBackground(this.backgroundColor)
+
+      if(!flag){
+          //ctx.rect(0, y, canvas2d.width, canvas2d.height);
+          ctx.fillStyle = colorList[0];
+          if(y <= 0){
+            this.backgroundColor = colorList[0];
+          }
+          ctx.fillRect(0, y, canvas2d.width, canvas2d.height);
+        
+      }else{
+          ctx.fillStyle = colorList[1];
+          if(y <= 0){
+            this.backgroundColor = colorList[1];
+          }
+          ctx.fillRect(0, y, canvas2d.width, canvas2d.height);
+          
+        
+        //console.log((timestamp - last)/10, y);
+      }
+      ctx.fill();
+      console.log("dim: ", dim, "y: ", y);
+    
+
+      /*
+      y -= (timestamp - last) / 10;
+      last = timestamp;
+      */
+      y -= dim;
+      dim += 0.0001;
+      
+      ctx.fillStyle = "white";
+      ctx.fillText( new Date().toTimeString().split(' ')[0], canvas2d.width / 2, canvas2d.height / 2 );
+
+      requestAnimationFrame( this.anim.bind(this) );
+  },
+}
+ctx.font = "50px Arial";
+ctx.textAlign = "center";
+ctx.textBaseline = "middle";
+paint.anim();
